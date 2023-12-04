ff3666a641bb095b4da28333f0992952317c6653
Julien Benetti <julien.benetti@atempo.com>
2023-12-04 09:58:15 +0100

    refacto fn

--- day1/rust2/src/main.rs
+++ day1/rust2/src/main.rs
@@ -1,35 +1,27 @@
 use std::{fs, char};
 
+fn find_first_number<T: Iterator>(mut chars: T) -> char 
+where
+    T: Iterator<Item = char>,
+{
+    while let Some(c) = chars.next() {
+        if c.is_numeric() {
+            return c;
+        }
+    }
+    return '0';
+}
+
 fn main() {
     let input = fs::read_to_string("../inputs/exemple2").expect("Read error");
 
     let mut sum: i32 = 0;
     for line in input.lines() {
-        let mut first = '0';
-        let mut last = '0';
-        let mut substring: String = String::new();
-        let mut chars = line.chars();
-        while let Some(c) = chars.next() {
-            if c.is_numeric() {
-                first = c;
-                break;
-            }
-        }
-        let mut chars_rev = line.chars().rev();
-        while let Some(c) = chars_rev.next() {
-            if c.is_numeric() {
-                last = c;
-                break;
-            }
-        }
-    sum += format!("{}{}", first, last).parse::<i32>().unwrap();
+        // let mut substring: String = String::new();
+        let first = find_first_number(line.chars());
+        let last = find_first_number(line.chars().rev());
+        sum += format!("{}{}", first, last).parse::<i32>().unwrap();
     }
 
-
-    let mut sum = 0;
-    for line in input.lines() {
-        let nums: Vec<char> = line.chars().filter(|&c| c.is_numeric()).collect();
-        sum += format!("{}{}", nums.first().unwrap(), nums.last().unwrap()).parse::<i32>().unwrap();
-    }
     println!("{:?}", sum);
 }
