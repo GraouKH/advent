746e8b132a2dc675da5c8347ab450f1bb565d834
Julien Benetti <julien.benetti@atempo.com>
2023-12-04 11:07:08 +0100

    day one finish

--- day1/rust2/src/main.rs
+++ day1/rust2/src/main.rs
@@ -1,25 +1,34 @@
-use std::{fs, char};
+use std::{fs, char, collections::HashMap};
 
-fn find_first_number<T: Iterator>(mut chars: T) -> char 
+fn find_first_number<T: Iterator>(mut chars: T, hash: &HashMap<&str, char>) -> char 
 where
     T: Iterator<Item = char>,
 {
+    let mut substring: String = String::new();
     while let Some(c) = chars.next() {
         if c.is_numeric() {
             return c;
-        }
+        };
+        substring.push(c);
+        for (key, value) in hash {
+            if substring.ends_with(key) {
+                return *value;
+            }
+        };
     }
     return '0';
 }
 
 fn main() {
-    let input = fs::read_to_string("../inputs/exemple2").expect("Read error");
+    let input = fs::read_to_string("../inputs/input1").expect("Read error");
 
     let mut sum: i32 = 0;
+    let hash: HashMap<&str, char> = HashMap::from([("one", '1'),("two", '2'),("three", '3'),("four", '4'),("five", '5'),("six", '6'),("seven", '7'),("eight", '8'),("nine", '9')]);
+    let hash_rev: HashMap<&str, char> = HashMap::from([("eno", '1'),("owt", '2'),("eerht", '3'),("ruof", '4'),("evif", '5'),("xis", '6'),("neves", '7'),("thgie", '8'),("enin", '9')]);
+
     for line in input.lines() {
-        // let mut substring: String = String::new();
-        let first = find_first_number(line.chars());
-        let last = find_first_number(line.chars().rev());
+        let first = find_first_number(line.chars(), &hash);
+        let last = find_first_number(line.chars().rev(), &hash_rev);
         sum += format!("{}{}", first, last).parse::<i32>().unwrap();
     }
 
