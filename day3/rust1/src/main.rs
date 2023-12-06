127fbc78c52267559e893e0126ee56b8ba01931f
Julien Benetti <julien.benetti@atempo.com>
2023-12-06 14:01:33 +0100

    test with 3 lines, still some errors

--- day3/rust1/src/main.rs
+++ day3/rust1/src/main.rs
@@ -1,42 +1,39 @@
-use std::{fs, cmp::{max, min}};
+use std::{fs::File, cmp::{max, min}};
+use std::io::{BufRead, BufReader};
 
-fn is_symbol_around(three_lines: [&str; 3], start: isize, end: usize, length: usize) -> bool {
-    let (min, max) = (max(start - 1, 0) as usize, min(end + 1, length - 1));
-    three_lines.iter().any(|line| {
-        !line.is_empty() && line[min..=max]
-            .chars()
-            .any(|c| !c.is_numeric() && c != '.')
-    })
-
+fn is_symbol_around(previous_line: &str, current_line: &str, next_line: &str, start: isize, end: usize) -> bool {
+    let (min, max) = (max(start - 1, 0) as usize, min(end + 1, current_line.len() - 1));
+    let (start_char, end_char) = (current_line.chars().nth(min).unwrap(), current_line.chars().nth(max).unwrap());
+    previous_line[min..=max].chars().any(|c| !c.is_numeric() && c != '.') && next_line[min..=max].chars().any(|c| !c.is_numeric() && c != '.') && !start_char.is_numeric() && start_char != '.' && !end_char.is_numeric() && end_char != '.' 
 }
 
 fn main() {
-    // TODO : Not use read to string :(
-    let input = fs::read_to_string("../inputs/input").expect("Read error");
-    let mut iter_lines = input.lines().peekable();
+    let file = File::open("../inputs/input").expect("Read error");
+    let mut input_lines = BufReader::new(file).lines();
     let mut sum: usize = 0;
-
-    // TODO : https://stackoverflow.com/questions/62186871/how-to-correctly-use-peek-in-rust
 
     let mut start: isize  = 0;
     let mut is_numbering = false;
-    let mut previous_line = "";
-    while let Some(line) = iter_lines.next() {
-        for (i, c) in line.chars().enumerate() {
+    let mut previous_line = String::default();
+    let mut current_line = input_lines.next().unwrap().unwrap();
+    let mut next_line = input_lines.next().unwrap().unwrap();
+    while let Some(line) = input_lines.next() {
+        for (i, c) in current_line.chars().enumerate() {
             if c.is_numeric() {
                 if !is_numbering {
                     start = i as isize;
                     is_numbering = true;
                 }
             } else if is_numbering {
-                let three_lines: [&str; 3] = [previous_line, line, iter_lines.peek().unwrap_or(&"")];
-                if is_symbol_around(three_lines, start, i-1, line.len()) {
-                    sum += &line[start as usize..i].parse::<usize>().unwrap();
+                if is_symbol_around(&previous_line, &current_line, &next_line, start, i-1) {
+                    sum += &current_line[start as usize..i].parse::<usize>().unwrap();
                 }
                 is_numbering = false;
             }
         }
-        previous_line = &line;
+        previous_line = current_line;
+        current_line = next_line;
+        next_line = line.unwrap();
     }
     println!("{sum:}");
 }
