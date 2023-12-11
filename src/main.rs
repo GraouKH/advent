cc95b83828227caaf84ce968569a40c8d7d2956f
GraouKH <cubjulien@gmail.com>
2023-12-11 18:55:18 +0100

    day 5 changes

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,71 @@
 
-use std::fs::{File, self};
-use std::io::{BufRead, BufReader};
+use std::{fs, str::FromStr};
+
+struct Range {
+    new_start: usize,
+    range_start: usize,
+    range_length: usize
+}
 
 struct Cog {
-    range: Vec<usize>,
-    modifier: Vec<usize>
+    ranges: Vec<Range>
+}
+
+impl Cog {
+    fn compute(&self, mut x: usize) -> usize {
+        for r in &self.ranges {
+            x = if (r.range_start..=r.range_start + r.range_length).contains(&x) {&r.new_start + &r.range_start + &r.range_length - &x} else {x};
+            break;
+        }
+        x
+    }
 }
 struct Almanac {
     seeds: Vec<usize>,
     cogs: Vec<Cog>
 }
 
-impl Almanac {
-    fn points(&self) -> usize {
-        let count = self.winning_numbers.intersection(&self.numbers).count();
-        if count > 0 { 1 << count - 1 } else { 0 }
-    }
-}
-
 impl FromStr for Almanac {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let card_lists = s.split_once(":").unwrap().1.split_once("|").unwrap();
-
-        let seeds: Vec<usize> = card_lists.0
+        let mut almanac_lines: Vec<&str> = s.split("\r\n\r\n").collect();
+        let seed_line = almanac_lines[0];
+        let seeds: Vec<usize> = seed_line.split_once(":").unwrap().1
             .split_whitespace()
             .map(|r| r.trim().parse::<usize>().unwrap())
             .collect();
 
-        let cogs: Vec<Cog> = card_lists.1
-            .split_whitespace()
-            .map(|r| r.trim().parse::<usize>().unwrap())
-            .collect();
+        let cogs: Vec<Cog> = Vec::new();
+        almanac_lines[1..]
+        println!("{s:?}");
+        // let cogs: Vec<Cog> = card_lists.1
+        //     .split_whitespace()
+        //     .map(|r| r.trim().parse::<usize>().unwrap())
+        //     .collect();
         Ok(Almanac { seeds, cogs })
     }
 }
 
-fn main() {
-    let file = File::open("exemple").expect("Read error");
-    let input_lines = fs::read_to_string("exemple").expect("Read error");
-    
-    let almanac = input_lines.parse::<Almanac>().unwrap();
-    let mut locations: Vec<usize> = Vec::with_capacity(almanac.seeds.len());
-    for seed in almanac.seeds {
-        location.push(almanac.compute(seed))
+impl Almanac {
+    fn compute(&self) -> Vec<usize> {
+        let mut location = Vec::with_capacity(self.seeds.len());
+        for s in &self.seeds {
+            let mut l = *s;
+            for c in &self.cogs{
+                l = c.compute(l);
+            }
+            location.push(l);
+        }
+        location
     }
-    // let seed_line = input_lines.next().unwrap().unwrap();
-    // let seeds: Vec<usize> = seed_line.split_once(":").unwrap().1
-    //                                  .split_whitespace()
-    //                                  .map(|r| r.trim().parse::<usize>().unwrap())
-    //                                  .collect();
-    let location_min: usize = locations.iter().min(); 
+}
+fn main() {
+    let input = fs::read_to_string("exemple").expect("Read error");
+    
+    let almanac = input.parse::<Almanac>().unwrap();
+    let locations: Vec<usize> = almanac.compute();
+    
+    let location_min: usize = *locations.iter().min().unwrap(); 
     println!("{location_min:?}");
 }
