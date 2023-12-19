0225b11cfec49ec2b0a215711fdd81901b92a520
Julien Benetti <julien.benetti@atempo.com>
2023-12-19 16:51:42 +0100

    day8 part2 try

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     let number_instructions = instructions.len();
     input_lines.next();
     let mut nodes: HashMap<String, (String, String)> = HashMap::new();
+    let mut starts: Vec<String> = Vec::new();
 
     while let Some(line) = input_lines.next() {
         let re = Regex::new(r"^(\w+) = \((\w+), (\w+)\)$").unwrap();
@@ -18,20 +19,32 @@ fn main() {
             let left = n[2].to_string();
             let right = n[3].to_string();
             nodes.insert(name.clone(), (left, right));
+            if name.ends_with('A') {
+                starts.push(name);
+            }
         }
     }
-    
-    let mut current_node= "AAA";
+
+
+    println!("{}", starts.len());
     let mut count: usize = 0;
-    while current_node != "ZZZ" {
+    while starts.iter().any(|s| !s.ends_with('Z')) {
         let turn = instructions[count % number_instructions];
-        let current_pair = nodes.get(current_node).unwrap();
-        current_node = match turn {
-            'L' => &current_pair.0,
-            'R' => &current_pair.1,
-            _ => "",
-        };
+        let mut new_starts = Vec::new();
+        for current_node in starts {
+            let current_pair = nodes.get(&current_node).unwrap();
+            let new_node = match turn {
+                'L' => &current_pair.0,
+                'R' => &current_pair.1,
+                _ => "",
+            };
+            new_starts.push(new_node.to_string());
+        }
+        starts = new_starts; 
         count += 1;
+        if count % 1000000 == 0 {
+            println!("{count}");
+        }
     }
     println!("{count}");
 }
